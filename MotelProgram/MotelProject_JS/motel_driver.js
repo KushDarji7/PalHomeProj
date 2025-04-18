265cd5b4b030170be0152d4cc7765c6ee10d35ab
Kush <islayusim@live.com>
2025-04-18 14:36:27 -0400

    added date time and skeleton for object retrevial from DB

--- MotelProgram/MotelProject_JS/motel_driver.js
+++ MotelProgram/MotelProject_JS/motel_driver.js
@@ -1,21 +1,33 @@
-// 1.A1 system log daily TIll/Register,
-// 1.A2
-//  must print date= #day, month, dayofweek, year
-// 1.A3
-//  must print cash amount = typed in manually
-// 1.A4
-//  must print credited transcation amount = confirmed through daily audit and credit transcation report
-// 1.A5
-//  must print statement of all transaction of the day
-// 1.A6
-// transactions will be listed as such
-// Room number RoomHolderName / Credit purchaser / cash amount paid, credit amount paid
+// import guestObject_DB.js
 
-// 2.A What is a Day
-// 2.A1
-//  A day is a period from when one credit statement report starts and ends, 
-// that is 6am I think to 6am next day 24hr cycle
+const SuperIntendentMotel = () => {
+  const currentDate = new Date();
 
+ 
 
+  const dailyReport = ()=> {
+    console.log("Vergil $: " + currentDate );
+    
+    transactions();
+    
+  };
 
+  const transactions = () => {
 
+      console.log("no transactions");
+  }
+
+  const retriveVaccantRooms = () => {
+    // read values and keys of guest objects that have been put into a array of vaccant rooms and non vaccant rooms,
+    // prints out the vaccant rooms from the vaccant rooms array
+
+  }
+
+  const listRooms = () => {
+    console.log("Printing Out rooms");
+  }
+
+  dailyReport();
+}
+
+SuperIntendentMotel();
