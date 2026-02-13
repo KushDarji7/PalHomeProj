f9ec84d3bc2975efb7135a9ed868130cd1894340
Kush D <60727944+Palpeleno@users.noreply.github.com>
2026-02-13 17:09:38 -0500

    added menu buttons, that can be clicked with cursor icon change, size looks good, color can be changed

--- MotelwDB/hotel.py
+++ MotelwDB/hotel.py
@@ -39,11 +39,93 @@ class HotelManagementSystem:
             bd=4,
             relief=RIDGE,
         )
-        lbl_title.place(x=0,y=180,width=1440, height=50)
+        lbl_title.place(x=0, y=180, width=1440, height=50)
 
         # main frame  =======================================================
-        main_frame = Frame(self.root,bd=4,relief=RIDGE)
-        main_frame.place(x=0,y=230, width=1440, height=670)
+        main_frame = Frame(self.root, bd=4, relief=RIDGE)
+        main_frame.place(x=0, y=180, width=1440, height=680)
+
+        # menu ==============================================================
+        # label for menu , and location in root window
+        lblmenu = Label(
+            main_frame,
+            text="MENU",
+            font=("times new roman", 20, "bold"),
+            bg="black",
+            fg="silver",
+            bd=4,
+            relief=RIDGE,
+        )
+        lblmenu.place(
+            x=0,
+            y=1,
+            width=214,
+        )
+
+        # button frame  =======================================================
+        button_frame = Frame(main_frame, bd=4, relief=RIDGE)
+        button_frame.place(x=0, y=41, width=214, height=195)
+
+        cust_btn = Button(
+            button_frame,
+            text="CUSTOMER",
+            width=18,
+            font=("times new roman", 14, "bold"),
+            bg="black",
+            fg="silver",
+            bd=0,
+            cursor="hand1",
+        )
+        cust_btn.grid(row=0, column=0, pady=1)
+
+        room_btn = Button(
+            button_frame,
+            text="ROOM",
+            width=18,
+            font=("times new roman", 14, "bold"),
+            bg="black",
+            fg="silver",
+            bd=0,
+            cursor="hand1",
+        )
+        room_btn.grid(row=1, column=0, pady=1)
+
+        details_btn = Button(
+            button_frame,
+            text="DETAILS ",
+            width=18,
+            font=("times new roman", 14, "bold"),
+            bg="black",
+            fg="silver",
+            bd=0,
+            cursor="hand1",
+        )
+        details_btn.grid(row=2, column=0, pady=1)
+
+        report_btn = Button(
+            button_frame,
+            text="REPORT",
+            width=18,
+            font=("times new roman", 14, "bold"),
+            bg="black",
+            fg="silver",
+            bd=0,
+            cursor="hand1",
+        )
+        report_btn.grid(row=3, column=0, pady=1)
+
+        logout_btn = Button(
+            button_frame,
+            text="LOGOUT",
+            width=18,
+            font=("times new roman", 14, "bold"),
+            bg="black",
+            fg="silver",
+            bd=0,
+            cursor="hand1",
+        )
+        logout_btn.grid(row=4, column=0, pady=1)
+
 
 # call object for window UI instantiating the object
 if __name__ == "__main__":
--- MotelwDB/readme.md
+++ MotelwDB/readme.md
@@ -4,3 +4,10 @@ imports library from Tikinter toolket for window management
 and pillow library
 use 
     pip install pillow
+
+
+
+<!-- CHANGES TO DO  -->
+
+create variable folder for colors and make the variables changes the color labels for each asset, so that they can be changed.
+
