26ac514c14a2df38bccaea1a3c70aac80135df01
Kush <islayusim@live.com>
2026-02-26 15:53:55 -0500

    made CLI casino slot machine, prints icons, random syombol picks betting. To do : winning, losting

---
+++ gamblingGame/main.py
@@ -0,0 +1,131 @@
+import random 
+
+# slot machine, 3 in a row matches, 3x3
+
+# constant value of lines in slot machine/
+# or rows of char that cycle in slot machine
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+SLOT_ROWS = 3
+SLOT_COLS = 3
+
+symbol_count = {
+    "🍑" : 2,
+    "🃏" : 3,
+    "💣" : 6,
+    "🍒" : 8
+}
+
+# def check_winnings(columns, lines, bet):
+
+
+# GENERATE  what symbols will be in cols
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        # _ key words when you are iterating, and it doesn't matter we use a anonymous value
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    # adds a copy to this variable instead of a reference to prevent overwriting of either variables
+    columns = [] 
+    for _ in range(cols):
+        # what values go into columns vertically, per rows
+        column = []
+        # : is there to make a reference not a copy
+        current_symbols = all_symbols[:]
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            # remove selected symbols, from finding first instance in the list
+            current_symbols.remove(value)
+            column.append(value)
+            # add value of symbol to column
+        columns.append(column)
+
+    return columns
+
+def print_slots_machine(columns):
+    '''
+    columns displayed and function as row as the data structure
+    we need to transpose the matrix 90 so its vertical instead of horizontal
+    if crash its because the index is out of range 
+    '''
+    for row in range(len(columns[0])):
+        '''
+        loop through all columns and only print first value of index of current row
+        enumeration gives index as well as the item in the loop 
+        '''
+        for i, column in enumerate(columns):
+            if i != len(columns) - 1:
+                print(column[row], end=" | ")
+            else:
+                print(column[row], end="")
+        print() # new line after each row
+
+def deposit(): 
+    while True:
+        amount = input("What is your deposit? \n$: ")
+        # input query check, convert string to int
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+# collect bet
+def get_num_of_lines():
+    while True:
+        lines = input("enter number of lines to bet on [1 -> " + str(MAX_LINES) + "]? \n: ")
+        # input query check, convert string to int
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("enter valid number of lines")
+        else:
+            print("Please enter a number.")
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What is your bet, on each line? \n $: ")
+        # input query check, convert string to int
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"amount must be between ${MIN_BET} <-> ${MAX_BET}.")
+        else:
+            print("Please enter a number.")
+    return amount
+
+def main():
+    balance = deposit()
+    lines = get_num_of_lines()
+    # check if bet balance fit bet lines 
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+        
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+    
+    print(f"You're betting ${bet} on {lines} lines.\n Total bet is equal to: ${total_bet}")
+
+    print("Balance:", balance, "Bet lines:", lines)
+
+    slots = get_slot_machine_spin(SLOT_ROWS,SLOT_COLS, symbol_count)
+    print_slots_machine(slots)
+
+main()
+ 
